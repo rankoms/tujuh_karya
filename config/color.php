f3fab2d18edcc26cd3d3e77635c478f7c7e6a77a
rankoms <randy.komara@r17.co.id>
2023-03-17 18:19:27 +0700

    menambahkan css

---
+++ config/color.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'color_utama' => '#A200BC',
+    'color_button_utama' => '#FFF',
+    'color_button_secondary' => '#4D47C3',
+    'gradient_color' => 'linear-gradient(311.76deg, #948cff80 -15.24%, #FFFFFF 78.85%)',
+    'color_utama_soft' => '#e5e3ff',
+
+    'class_color' => [
+        'primary' => 'primary',
+        'secondary' => 'secondary',
+        'success' => 'success',
+        'danger' => 'danger',
+        'warning' => 'warning',
+        'info' => 'info',
+        'light' => 'light',
+        'dark' => 'dark',
+        'white' => 'white'
+    ]
+];
--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -8,18 +8,10 @@
 
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up"
+				<div class="col-lg-12 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up"
 					data-aos-delay="200">
-					<h1>Better Solutions For Your Business</h1>
-					<h2>We are team of talented designers making websites with Bootstrap</h2>
-					<div class="d-flex justify-content-center justify-content-lg-start">
-						<a href="#about" class="btn-get-started scrollto">Get Started</a>
-						<a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i
-								class="bi bi-play-circle"></i><span>Watch Video</span></a>
-					</div>
-				</div>
-				<div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-					<img src="{{ asset('landing/img/hero-img.png') }}" class="img-fluid animated" alt="">
+					<h2>WE CREATE EVENT</h2>
+					<h1>" EXTRADONARY "</h1>
 				</div>
 			</div>
 		</div>
@@ -28,68 +20,19 @@
 
 	<main id="main">
 
-		<!-- ======= Clients Section ======= -->
-		<section id="clients" class="clients section-bg">
-			<div class="container">
-
-				<div class="row" data-aos="zoom-in">
-
-					<div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-						<img src="{{ asset('landing/img/clients/client-1.png') }}" class="img-fluid" alt="">
-					</div>
-
-					<div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-						<img src="{{ asset('landing/img/clients/client-2.png') }}" class="img-fluid" alt="">
-					</div>
-
-					<div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-						<img src="{{ asset('landing/img/clients/client-3.png') }}" class="img-fluid" alt="">
-					</div>
-
-					<div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-						<img src="{{ asset('landing/img/clients/client-4.png') }}" class="img-fluid" alt="">
-					</div>
-
-					<div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-						<img src="{{ asset('landing/img/clients/client-5.png') }}" class="img-fluid" alt="">
-					</div>
-
-					<div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-						<img src="{{ asset('landing/img/clients/client-6.png') }}" class="img-fluid" alt="">
-					</div>
-
-				</div>
-
-			</div>
-		</section><!-- End Cliens Section -->
 
 		<!-- ======= About Us Section ======= -->
 		<section id="about" class="about">
 			<div class="container" data-aos="fade-up">
-
-				<div class="section-title">
-					<h2>About Us</h2>
-				</div>
+				<h3>WELCOME TO TUJUH KARYA SINERGI</h3>
 
 				<div class="row content">
-					<div class="col-lg-6">
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-							magna aliqua.
-						</p>
-						<ul>
-							<li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
-							<li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
-							<li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
-						</ul>
-					</div>
-					<div class="col-lg-6 pt-4 pt-lg-0">
-						<p>
-							Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-							velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-							culpa qui officia deserunt mollit anim id est laborum.
-						</p>
-						<a href="#" class="btn-learn-more">Learn More</a>
+					<div class="col-lg-12 p-4">
+						<h2>
+							Lorem Ipsum for your design, website, and copywriting works. Use our tool to generate chunks of text that is free
+							from repetition and copyright claims. 3D Images Increase Sales.
+						</h2>
+						<h1>" We create great events "</h1>
 					</div>
 				</div>
 
