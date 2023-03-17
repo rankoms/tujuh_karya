87c437822f4de9a97172c37030359acba2dd782a
rankoms <randy.komara@r17.co.id>
2023-03-18 00:57:47 +0700

    membuat sampai services

--- resources/views/landing/header.blade.php
+++ resources/views/landing/header.blade.php
@@ -4,17 +4,17 @@
 
 		<!-- Uncomment below if you prefer to use an image logo -->
 		<div class="d-flex align-items-center">
-			{{-- <a href="{{ route('index') }}" class="logo me-2 ms-4"><img src="{{ asset('logo.png') }}" alt=""
-					class="img-fluid"></a> --}}
-			<a href="{{ route('index') }}" class="logo me-2 ms-4"><img src="{{ asset('landing/img/logo.png') }}" alt=""
+			<a href="{{ route('index') }}" class="logo me-2 ms-4"><img src="{{ asset('logo.png') }}" alt=""
 					class="img-fluid"></a>
+			{{-- <a href="{{ route('index') }}" class="logo me-2 ms-4"><img src="{{ asset('landing/img/logo.png') }}" alt=""
+					class="img-fluid"></a> --}}
 
 		</div>
 		<nav id="navbar" class="navbar ms-4">
 			<ul>
 				<li><a class="nav-link scrollto active" href="#hero">Home</a></li>
 				<li><a class="nav-link scrollto" href="#about">About</a></li>
-				<li><a class="nav-link scrollto" href="#package">Package</a></li>
+				<li><a class="nav-link scrollto" href="#services">Services</a></li>
 				<li><a class="nav-link scrollto" href="#apps">Apps</a></li>
 				<li><a class="nav-link scrollto" href="#team">Contact</a></li>
 			</ul>
--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -27,7 +27,8 @@
 				<h3>WELCOME TO TUJUH KARYA SINERGI</h3>
 
 				<div class="row content">
-					<div class="col-lg-12 p-4">
+					<div class="col-lg-12 p-4
+					">
 						<h2>
 							Lorem Ipsum for your design, website, and copywriting works. Use our tool to generate chunks of text that is free
 							from repetition and copyright claims. 3D Images Increase Sales.
@@ -39,178 +40,119 @@
 			</div>
 		</section><!-- End About Us Section -->
 
-		<!-- ======= Why Us Section ======= -->
-		<section id="why-us" class="why-us section-bg">
-			<div class="container-fluid" data-aos="fade-up">
-
-				<div class="row">
-
-					<div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
-
-						<div class="content">
-							<h3>Eum ipsam laborum deleniti <strong>velit pariatur architecto aut nihil</strong></h3>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-								magna aliqua. Duis aute irure dolor in reprehenderit
-							</p>
-						</div>
-
-						<div class="accordion-list">
-							<ul>
-								<li>
-									<a data-bs-toggle="collapse" class="collapse" data-bs-target="#accordion-list-1"><span>01</span> Non
-										consectetur a erat nam at lectus urna duis? <i class="bx bx-chevron-down icon-show"></i><i
-											class="bx bx-chevron-up icon-close"></i></a>
-									<div id="accordion-list-1" class="collapse show" data-bs-parent=".accordion-list">
-										<p>
-											Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida.
-											Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
-										</p>
-									</div>
-								</li>
-
-								<li>
-									<a data-bs-toggle="collapse" data-bs-target="#accordion-list-2" class="collapsed"><span>02</span> Feugiat
-										scelerisque varius morbi enim nunc? <i class="bx bx-chevron-down icon-show"></i><i
-											class="bx bx-chevron-up icon-close"></i></a>
-									<div id="accordion-list-2" class="collapse" data-bs-parent=".accordion-list">
-										<p>
-											Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec
-											ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper
-											dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
-										</p>
-									</div>
-								</li>
-
-								<li>
-									<a data-bs-toggle="collapse" data-bs-target="#accordion-list-3" class="collapsed"><span>03</span> Dolor sit
-										amet consectetur adipiscing elit? <i class="bx bx-chevron-down icon-show"></i><i
-											class="bx bx-chevron-up icon-close"></i></a>
-									<div id="accordion-list-3" class="collapse" data-bs-parent=".accordion-list">
-										<p>
-											Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer
-											enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna
-											duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
-										</p>
-									</div>
-								</li>
-
-							</ul>
-						</div>
-
-					</div>
-
-					<div class="col-lg-5 align-items-stretch order-1 order-lg-2 img"
-						style='background-image: url("{{ asset('landing/img/why-us.png') }}");' data-aos="zoom-in" data-aos-delay="150">
-						&nbsp;</div>
-				</div>
-
-			</div>
-		</section><!-- End Why Us Section -->
-
-		<!-- ======= Skills Section ======= -->
-		<section id="skills" class="skills">
-			<div class="container" data-aos="fade-up">
-
-				<div class="row">
-					<div class="col-lg-6 d-flex align-items-center" data-aos="fade-right" data-aos-delay="100">
-						<img src="{{ asset('landing/img/skills.png') }}" class="img-fluid" alt="">
-					</div>
-					<div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left" data-aos-delay="100">
-						<h3>Voluptatem dignissimos provident quasi corporis voluptates</h3>
-						<p class="fst-italic">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-							magna aliqua.
-						</p>
-
-						<div class="skills-content">
-
-							<div class="progress">
-								<span class="skill">HTML <i class="val">100%</i></span>
-								<div class="progress-bar-wrap">
-									<div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-									</div>
-								</div>
-							</div>
-
-							<div class="progress">
-								<span class="skill">CSS <i class="val">90%</i></span>
-								<div class="progress-bar-wrap">
-									<div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
-									</div>
-								</div>
-							</div>
-
-							<div class="progress">
-								<span class="skill">JavaScript <i class="val">75%</i></span>
-								<div class="progress-bar-wrap">
-									<div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-									</div>
-								</div>
-							</div>
-
-							<div class="progress">
-								<span class="skill">Photoshop <i class="val">55%</i></span>
-								<div class="progress-bar-wrap">
-									<div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100">
-									</div>
-								</div>
-							</div>
-
-						</div>
-
-					</div>
-				</div>
-
-			</div>
-		</section><!-- End Skills Section -->
-
 		<!-- ======= Services Section ======= -->
 		<section id="services" class="services section-bg">
 			<div class="container" data-aos="fade-up">
 
-				<div class="section-title">
-					<h2>Services</h2>
-					<p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
-						consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in
-						iste officiis commodi quidem hic quas.</p>
-				</div>
+				<h3>WHAT WE DO</h3>
+				<h2>We do it at its best</h2>
+				<p class="p-title">We are the pioneer and leading company in managing and organizing an international music festival
+					for years in
+					Indonesia. We are an expert in executing world-class and memorable events. Collaborate with us, and we will exceed
+					your expectation.</p>
 
 				<div class="row">
-					<div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+					<div class="col-xl-6 col-md-6 d-flex align-items-stretch mt-4 mt-md-0 mb-4" data-aos="zoom-in" data-aos-delay="100">
 						<div class="icon-box">
-							<div class="icon"><i class="bx bxl-dribbble"></i></div>
-							<h4><a href="">Lorem Ipsum</a></h4>
-							<p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+							<div class="icon text-center"><img src="{{ asset('img/ticket.svg') }}" width="auto" alt="thumbnail"></div>
+							<h4>TICKETING PLATFORM AND REGISTRATION SYSTEM</h4>
+							<p>We are the pioneer and leading company in managing and organizing an international music festival for years in
+								Indonesia. We are an expert in executing world-class and memorable events. Collaborate with us, and we will
+								exceed your expectation.</p>
 						</div>
 					</div>
 
-					<div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
-						<div class="icon-box">
-							<div class="icon"><i class="bx bx-file"></i></div>
-							<h4><a href="">Sed ut perspici</a></h4>
-							<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+					<div class="col-xl-6 col-md-6 d-flex align-items-stretch mt-4 mt-md-0 mb-4" data-aos="zoom-in" data-aos-delay="200">
+						<div class="icon-box text-center">
+							<div class="icon"><img src="{{ asset('img/star.svg') }}" width="auto" alt="thumbnail"></div>
+							<h4>ARTIST/TALENT BOOKING</h4>
+							<p>Do you need live entertainment for your event? Many years of experience in this field, we have worked with many
+								talents, from emerging new talents/artists to well-known talents/artists. We maintain our relationships well and
+								with that, we can help you to create impressive events.</p>
 						</div>
 					</div>
 
-					<div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
-						<div class="icon-box">
-							<div class="icon"><i class="bx bx-tachometer"></i></div>
-							<h4><a href="">Magni Dolores</a></h4>
-							<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+					<div class="col-xl-6 col-md-6 d-flex align-items-stretch mt-4 mt-md-0 mb-4" data-aos="zoom-in" data-aos-delay="300">
+						<div class="icon-box text-center">
+							<div class="icon"><img src="{{ asset('img/partner.svg') }}" width="auto" alt="thumbnail"></div>
+							<h4><a href="">SPONSORSHIP AND PARTNERSHIP</a></h4>
+							<p>With our strong relationship and extensive experience working with various companies, we can help to provide
+								you with marketing services for your events.</p>
 						</div>
 					</div>
 
-					<div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
-						<div class="icon-box">
-							<div class="icon"><i class="bx bx-layer"></i></div>
-							<h4><a href="">Nemo Enim</a></h4>
-							<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+					<div class="col-xl-6 col-md-6 d-flex align-items-stretch mt-4 mt-md-0 mb-4" data-aos="zoom-in" data-aos-delay="400">
+						<div class="icon-box text-center">
+							<div class="icon"><img src="{{ asset('img/event.svg') }}" width="auto" alt="thumbnail"></div>
+							<h4>EVENTS</h4>
+							<p>We began our journey in 2005 by creating one of the largest jazz festivals in the world. As years go by, we
+								continue to grow and with our experience, we will provide you with professional insights and help to execute your
+								event smoothly.</p>
 						</div>
 					</div>
 
 				</div>
 
+			</div>
+
+			<div class="container" data-aos="fade-up">
+
+				<h3>OUR APPROACH</h3>
+				<h2>Idea. Develop. Execute.</h2>
+
+				<div class="row">
+					<div class="col-xl-4 col-sm-12 d-flex mt-4 mb-4 justify-content-center" data-aos="zoom-in" data-aos-delay="100">
+						<div class="icon-box">
+							<div class="angka">52</div>
+							<h5>EVENT LAUNCHED</h5>
+						</div>
+					</div>
+					<div class="col-xl-4 col-sm-12 d-flex mt-4 mb-4 justify-content-center" data-aos="zoom-in" data-aos-delay="100">
+						<div class="icon-box">
+							<div class="angka">1,250</div>
+							<h5>WORKING HOURS</h5>
+						</div>
+					</div>
+					<div class="col-xl-4 col-sm-12 d-flex mt-4 mb-4 justify-content-center" data-aos="zoom-in" data-aos-delay="100">
+						<div class="icon-box">
+							<div class="angka">124</div>
+							<h5>SATISFIED CLIENTS</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="why-choose-me" data-aos="fade-up">
+				<h3>WHY CHOOSE TUJUH KARYA SINERGI PRODUCTION?</h3>
+				<h2>We listen. We discuss. <br />We advise.We Provide.</h2>
+
+				<div class="row">
+					<div class="col-xl-4 col-sm-12 d-flex mt-4 mb-4 justify-content-center" data-aos="zoom-in" data-aos-delay="100">
+						<div class="icon-box">
+							<div class="icon text-center"><img src="{{ asset('img/creative.svg') }}" width="auto" alt="thumbnail"></div>
+							<h4>We are creative
+							</h4>
+							<p>One of the keys to a successful event is a creative mind. Our team is full of passion, we crave for challenges
+								that gives excitement, and ready to have a creative discussion with you to produce a splendid event.</p>
+						</div>
+					</div>
+					<div class="col-xl-4 col-sm-12 d-flex mt-4 mb-4 justify-content-center" data-aos="zoom-in" data-aos-delay="100">
+						<div class="icon-box">
+							<div class="icon text-center"><img src="{{ asset('img/listener.svg') }}" width="auto" alt="thumbnail"></div>
+							<h4>We are a good listener</h4>
+							<p>Our team is filled with people passionate about events. Share your thoughts and we will help to develop the
+								concept, design, and execute your event from beginning to end.</p>
+						</div>
+					</div>
+					<div class="col-xl-4 col-sm-12 d-flex mt-4 mb-4 justify-content-center" data-aos="zoom-in" data-aos-delay="100">
+						<div class="icon-box">
+							<div class="icon text-center"><img src="{{ asset('img/time.svg') }}" width="auto" alt="thumbnail"></div>
+							<h4>We deliver on time</h4>
+							<p>Time is the most essential part of crafting an event. We are ready to meet your deadlines in executing an event
+								that is astounding.</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		</section><!-- End Services Section -->
 
