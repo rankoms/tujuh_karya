df69bc66d7652b78c72943c5737bba2d46c443b8
rankoms <randy.komara@r17.co.id>
2023-04-02 09:31:14 +0700

    fix galery

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -29,10 +29,20 @@
 				<div class="row content">
 					<div class="col-lg-12 p-4
 					">
-						<h4 style="text-align:justify;">Didirikan pada tahun 2022, PT. Tujuh Karya Sinergi bergerak di bidang promotor, event organizer, dan periklanan yang memiliki tujuan memasarkan dunia hiburan di Indonesia. Pada tahun 2019, sebelum perusahaan diresmikan oleh Badan Hukum, kami sudah mengadakan festival untuk pertama kali nya yang dikenal sebagai Pesta Semalam Minggu Vol.1, yang dimana akar dari munculnya festival lanjutan seperti Pesta Semalam Minggu Vol.2, Pesta Semalam Minggu “Pesta Lagi 2022, Pesta Semalam Minggu Vol.3 hingga Pesta Semalam Minggu “Pesta Lagi” 2023.</h4></br>
-						<h4 style="text-align:justify;">Tujuh Karya Sinergi kini telah konsisten menjalankan 6 event terhitung sejak Maret 2023, dan akan terus menyelenggarakan Festival Musik dengan terus meningkatan kualitas untuk menghibur masyarakat luas.</h4></br>
-						<h4 style="text-align:justify;">Pada intinya Tujuh Karya Sinergi bertujuan untuk mempromosikan Dunia Hiburan terutama Festival Musik, kami terus berupaya mempresentasikan hiburan yang berkualitas dan tersusun dengan baik, sekaligus memantapkan diri sebagai perusahaan yang diakui oleh masyarakat untuk menyelenggarakan acara secara nasional maupun internasional.</h4></br>
-						</div>
+						<h4 style="text-align:justify;">Didirikan pada tahun 2022, PT. Tujuh Karya Sinergi bergerak di bidang promotor,
+							event organizer, dan periklanan yang memiliki tujuan memasarkan dunia hiburan di Indonesia. Pada tahun 2019,
+							sebelum perusahaan diresmikan oleh Badan Hukum, kami sudah mengadakan festival untuk pertama kali nya yang dikenal
+							sebagai Pesta Semalam Minggu Vol.1, yang dimana akar dari munculnya festival lanjutan seperti Pesta Semalam Minggu
+							Vol.2, Pesta Semalam Minggu “Pesta Lagi 2022, Pesta Semalam Minggu Vol.3 hingga Pesta Semalam Minggu “Pesta Lagi”
+							2023.</h4></br>
+						<h4 style="text-align:justify;">Tujuh Karya Sinergi kini telah konsisten menjalankan 6 event terhitung sejak Maret
+							2023, dan akan terus menyelenggarakan Festival Musik dengan terus meningkatan kualitas untuk menghibur masyarakat
+							luas.</h4></br>
+						<h4 style="text-align:justify;">Pada intinya Tujuh Karya Sinergi bertujuan untuk mempromosikan Dunia Hiburan
+							terutama Festival Musik, kami terus berupaya mempresentasikan hiburan yang berkualitas dan tersusun dengan baik,
+							sekaligus memantapkan diri sebagai perusahaan yang diakui oleh masyarakat untuk menyelenggarakan acara secara
+							nasional maupun internasional.</h4></br>
+					</div>
 				</div>
 
 			</div>
@@ -44,24 +54,18 @@
 			<img src="{{ asset('img/floating/floating_2.png') }}" alt="Floating 2" class="position-absolute floating-2">
 			<div class="container" data-aos="fade-up">
 				<h2>KAMI MELAKUKANNYA</br>DENGAN SEBAIK-BAIKNYA</h2>
-				<p class="p-title">Kami berusaha untuk menjadi perusahaan ternama dalam mengelola dan menyelenggarakan festival musik dalam taraf nasional hingga internasional. Kami ahli dalam melaksanakan acara yang berkesan. </p>
+				<p class="p-title">Kami berusaha untuk menjadi perusahaan ternama dalam mengelola dan menyelenggarakan festival musik
+					dalam taraf nasional hingga internasional. Kami ahli dalam melaksanakan acara yang berkesan. </p>
 
 				<div class="row">
-					<!-- <div class="col-xl-6 col-md-6 d-flex align-items-stretch mt-4 mt-md-0 mb-4" data-aos="zoom-in" data-aos-delay="100">
-						<div class="icon-box">
-							<div class="icon text-center"><img src="{{ asset('img/ticket.svg') }}" width="auto" alt="thumbnail"></div>
-							<h4>TICKETING PLATFORM AND REGISTRATION SYSTEM</h4>
-							<p>We are the pioneer and leading company in managing and organizing an international music festival for years in
-								Indonesia. We are an expert in executing world-class and memorable events. Collaborate with us, and we will
-								exceed your expectation.</p>
-						</div>
-					</div> -->
 
-					<div class="col-xl-12 col-md-12 d-flex align-items-stretch mt-4 mt-md-0 mb-4" data-aos="zoom-in" data-aos-delay="200">
+					<div class="col-xl-12 col-md-12 d-flex align-items-stretch mt-4 mt-md-0 mb-4" data-aos="zoom-in"
+						data-aos-delay="200">
 						<div class="icon-box text-center">
 							<div class="icon"><img src="{{ asset('img/star.svg') }}" width="auto" alt="thumbnail"></div>
 							<h4>PEMESANAN ARTIS/TALENTA</h4>
-							<p>Jika anda membutuhkan hiburan langsung untuk acara anda, kami siap membantu dengan pengalaman bertahun-tahun dalam bidang ini, kami telah bekerja sama dengan artis ternama. Kami menjaga relasi dengan baik, dengan </p>
+							<p>Jika anda membutuhkan hiburan langsung untuk acara anda, kami siap membantu dengan pengalaman bertahun-tahun
+								dalam bidang ini, kami telah bekerja sama dengan artis ternama. Kami menjaga relasi dengan baik, dengan </p>
 						</div>
 					</div>
 
@@ -69,7 +73,8 @@
 						<div class="icon-box text-center">
 							<div class="icon"><img src="{{ asset('img/partner.svg') }}" width="auto" alt="thumbnail"></div>
 							<h4><a href="">SPONSOR DAN KEMITRAAN</a></h4>
-							<p>Kami dapat membantu menyediakan kebutuhan pemasaran untuk acara anda melalui hubungan yang kuat dengan berbagai perusahaan. </p>
+							<p>Kami dapat membantu menyediakan kebutuhan pemasaran untuk acara anda melalui hubungan yang kuat dengan berbagai
+								perusahaan. </p>
 						</div>
 					</div>
 
@@ -77,7 +82,9 @@
 						<div class="icon-box text-center">
 							<div class="icon"><img src="{{ asset('img/event.svg') }}" width="auto" alt="thumbnail"></div>
 							<h4>ACARA</h4>
-							<p>Kami memulai perjalanan kami di tahun 2019 dengan membuat salah satu festival musik di Kota Bekasi. Seiring berjalannya waktu, kami terus berkembang dan dengan pengalaman kami, kami akan memberikan pengetahuan secara profesional yang akan membantu Anda untuk melaksanakan sebuah acara.  </p>
+							<p>Kami memulai perjalanan kami di tahun 2019 dengan membuat salah satu festival musik di Kota Bekasi. Seiring
+								berjalannya waktu, kami terus berkembang dan dengan pengalaman kami, kami akan memberikan pengetahuan secara
+								profesional yang akan membantu Anda untuk melaksanakan sebuah acara. </p>
 						</div>
 					</div>
 
@@ -85,45 +92,6 @@
 
 			</div>
 		</section>
-		<!-- <section class="services position-relative pt-0">
-
-			<img src="{{ asset('img/floating/floating_3.png') }}" alt="Floating 1" class="position-absolute floating-3 d-none">
-			<div class="container" data-aos="fade-up">
-
-				<h3>OUR APPROACH</h3>
-				<h2>Idea. Develop. Execute.</h2>
-
-				<div class="row">
-					<div class="col-xl-4 col-sm-12 d-flex mt-4 mb-4 justify-content-center" data-aos="zoom-in" data-aos-delay="100">
-						<div class="icon-box">
-							<div class="angka">52</div>
-							<h5>EVENT LAUNCHED</h5>
-						</div>
-					</div>
-					<div class="col-xl-4 col-sm-12 d-flex mt-4 mb-4 justify-content-center" data-aos="zoom-in" data-aos-delay="100">
-						<div class="icon-box">
-							<div class="angka">1,250</div>
-							<h5>WORKING HOURS</h5>
-						</div>
-					</div>
-					<div class="col-xl-4 col-sm-12 d-flex mt-4 mb-4 justify-content-center" data-aos="zoom-in" data-aos-delay="100">
-						<div class="icon-box">
-							<div class="angka">124</div>
-							<h5>SATISFIED CLIENTS</h5>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section> -->
-
-		<!-- <section class="services position-relative pt-0">
-			<div class="why-choose-me" data-aos="fade-up">
-				<h3>KENAPA HARUS TUJUH KARYA SINERGI ?</h3>
-				<h2>Kami bertujuan untuk membangun ekosistem entertaiment berjalan dengan baik & membesarkan event kami menjadi event yang lebih baik, tidak hanya di Bekasi tapi di kota lain juga.</h2>
-
-				
-			</div>
-		</section>End Services Section -->
 
 
 		<!-- ======= Portfolio Section ======= -->
@@ -137,7 +105,7 @@
 
 					<div class="row custom-portfolio" data-aos="fade-up" data-aos-delay="200">
 
-					<div class="col-lg-2 col-sm-12 offset-lg-1 p-0">
+						<div class="col-lg-2 col-sm-2 offset-lg-1 p-0">
 							<div class="row">
 								<div class="col-12 ">
 									<a href="{{ asset('img/portfolio/images_1.png') }}" data-gallery="portfolioGallery"
@@ -147,9 +115,9 @@
 								</div>
 							</div>
 						</div>
-						<div class="col-lg-6 col-sm-12">
+						<div class="col-lg-6 col-sm-6">
 							<div class="row">
-								<div class="col-lg-4 col-sm-12 p-0">
+								<div class="col-lg-4 col-sm-4 p-0">
 									<div class="row">
 
 										<div class="col-lg-12 col-sm-12 short-div">
@@ -167,7 +135,7 @@
 										</div>
 									</div>
 								</div>
-								<div class="col-lg-8 col-sm-12 p-0">
+								<div class="col-lg-8 col-sm-8 p-0">
 									<a href="{{ asset('img/portfolio/images_4.png') }}" data-gallery="portfolioGallery"
 										class="portfolio-lightbox preview-link portfolio-img"><img src="{{ asset('img/portfolio/images_4.png') }}"
 											class="img-fluid" alt="">
@@ -175,7 +143,7 @@
 								</div>
 							</div>
 						</div>
-						<div class="col-lg-2 col-sm-12 p-0">
+						<div class="col-lg-2 col-sm-2 p-0">
 							<div class="row">
 								<div class="col-sm-12">
 
@@ -194,7 +162,7 @@
 
 					<div class="row custom-portfolio" data-aos="fade-up" data-aos-delay="200">
 
-						<div class="col-lg-2 col-sm-12 p-0 offset-lg-1 ">
+						<div class="col-lg-2 col-sm-2 p-0 offset-lg-1 ">
 							<div class="row">
 								<div class="col-sm-12">
 									<a href="{{ asset('img/portfolio/images_7.png') }}" data-gallery="portfolioGallery"
@@ -208,15 +176,15 @@
 								</div>
 							</div>
 						</div>
-						<div class="col-lg-6 col-sm-12">
+						<div class="col-lg-6 col-sm-6">
 							<div class="row">
-								<div class="col-lg-8 col-sm-12 p-0">
+								<div class="col-lg-8 col-sm-8 p-0">
 									<a href="{{ asset('img/portfolio/images_9.png') }}" data-gallery="portfolioGallery"
 										class="portfolio-lightbox preview-link portfolio-img"><img src="{{ asset('img/portfolio/images_9.png') }}"
 											class="img-fluid" alt="">
 									</a>
 								</div>
-								<div class="col-lg-4 col-sm-12 p-0">
+								<div class="col-lg-4 col-sm-4 p-0">
 									<div class="row">
 
 										<div class="col-lg-12 col-sm-12 short-div">
@@ -235,7 +203,7 @@
 								</div>
 							</div>
 						</div>
-						<div class="col-lg-2 col-sm-12 p-0">
+						<div class="col-lg-2 col-sm-2 p-0">
 							<div class="row">
 								<div class="col-12 ">
 									<a href="{{ asset('img/portfolio/images_12.png') }}" data-gallery="portfolioGallery"
@@ -260,7 +228,7 @@
 
 				<div class="row" data-aos="zoom-in">
 
-					<div class="col-lg-2 col-md-6 col-6 d-flex align-items-center justify-content-center offset-lg-2">
+					<div class="col-lg-2 col-md-6 col-6 d-flex align-items-center justify-content-center">
 						<img src="{{ asset('img/client/1.png') }}" class="img-fluid" alt="Client">
 					</div>
 
